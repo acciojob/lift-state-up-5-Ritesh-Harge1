613668ccb422c157c0792aec930d969db697739a
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-16 08:43:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,1 @@
-import React from "react";
 
-function DummyApp() {
-  return (
-    <div>
-      <p>This is a placeholder App component inside src/components.</p>
-      <p>The real App.js lives in src/App.js 🚀</p>
-    </div>
-  );
-}
-
-export default DummyApp;
